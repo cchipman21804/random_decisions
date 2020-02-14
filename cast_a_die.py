37b6b135e609da7e04326e35cecabef4cdbcb8bb
cchipman21804 <61033936+cchipman21804@users.noreply.github.com>
2020-02-13 20:23:44 -0500

    Add files via upload

---
+++ cast_a_die.py
@@ -0,0 +1,273 @@
+# Cast A Die
+
+import random
+
+# Draw dice functions
+
+# ASCII Art by Joan G. Stark
+# https://www.asciiart.eu/miscellaneous/dice
+
+# Art by Joan G. Stark
+#       .-------.    ______
+#      /   o   /|   /\     \
+#     /_______/o|  /o \  o  \
+#     | o     | | /   o\_____\
+#     |   o   |o/ \o   /o    /
+#     |     o |/   \ o/  o  /
+#jgs  '-------'     \/____o/
+
+
+def zero():
+    # print("ZERO")
+    # Unicode box drawing
+    print("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510")
+    print("\u2502\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2502")
+    print("\u2502\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2502")
+    print("\u2502\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2502")
+    print("\u2522\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u252a")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2503\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u2503")
+    print("\u2517\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u251b")
+    print("\n")
+
+    # ASCII hashtag drawing
+    #    print("#########")
+    #    print("#       #")
+    #    print("#       #")
+    #    print("#       #")
+    #    print("#########")
+    #    print("\n")
+
+
+    return
+
+def one():
+    print(ONE)
+
+    # Unicode box drawing
+
+#    print("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510")
+#    print("\u2502       \u2502")
+#    print("\u2502   o   \u2502")
+#    print("\u2502       \u2502")
+#    print("\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518")
+#    print("\n")
+
+    # ASCII hashtag drawing
+
+#    print("#########")
+#    print("#       #")
+#    print("#   o   #")
+#    print("#       #")
+#    print("#########")
+#    print("\n")
+    return
+
+def two():
+    print(TWO)
+
+    # Unicode box drawing
+
+#    print("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510")
+#    print("\u2502 o     \u2502")
+#    print("\u2502       \u2502")
+#    print("\u2502     o \u2502")
+#    print("\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518")
+#    print("\n")
+
+    # ASCII hashtag drawing
+
+#    print("#########")
+#    print("# o     #")
+#    print("#       #")
+#    print("#     o #")
+#    print("#########")
+#    print("\n")
+    return
+
+def three():
+    print(THREE)
+
+    # Unicode box drawing
+
+#    print("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510")
+#    print("\u2502 o     \u2502")
+#    print("\u2502   o   \u2502")
+#    print("\u2502     o \u2502")
+#    print("\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518")
+#    print("\n")
+
+    # ASCII hashtag drawing
+
+#    print("#########")
+#    print("# o     #")
+#    print("#   o   #")
+#    print("#     o #")
+#    print("#########")
+#    print("\n")
+    return
+
+def four():
+    print(FOUR)
+
+    # Unicode box drawing
+
+#    print("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510")
+#    print("\u2502 o   o \u2502")
+#    print("\u2502       \u2502")
+#    print("\u2502 o   o \u2502")
+#    print("\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518")
+#    print("\n")
+
+    # ASCII hashtag drawing
+
+#    print("#########")
+#    print("# o   o #")
+#    print("#       #")
+#    print("# o   o #")
+#    print("#########")
+#    print("\n")
+    return
+
+def five():
+    print(FIVE)
+
+    # Unicode box drawing
+
+#    print("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510")
+#    print("\u2502 o   o \u2502")
+#    print("\u2502   o   \u2502")
+#    print("\u2502 o   o \u2502")
+#    print("\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518")
+#    print("\n")
+
+    # ASCII hashtag drawing
+
+#    print("#########")
+#    print("# o   o #")
+#    print("#   o   #")
+#    print("# o   o #")
+#    print("#########")
+#    print("\n")
+    return
+
+def six():
+    print(SIX)
+
+    # Unicode box drawing
+
+#    print("\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510")
+#    print("\u2502 o   o \u2502")
+#    print("\u2502 o   o \u2502")
+#    print("\u2502 o   o \u2502")
+#    print("\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518")
+#    print("\n")
+
+    # ASCII hashtag drawing
+
+#    print("#########")
+#    print("# o   o #")
+#    print("# o   o #")
+#    print("# o   o #")
+#    print("#########")
+#    print("\n")
+    return
+
+
+print("Cast A Die")
+print("ASCII Art by Joan G. Stark")
+print("https://www.asciiart.eu/miscellaneous/dice")
+
+# ASCII Art by Joan G. Stark
+# https://www.asciiart.eu/miscellaneous/dice
+titleart = '''
+       .-------.    ______
+      /   o   /|   /\\     \\
+     /_______/o|  /o \\  o  \\
+     | o     | | /   o\\_____\\
+     |   o   |o/ \\o   /o    /
+     |     o |/   \\ o/  o  /
+jgs  \'-------\'     \\/____o/
+'''
+
+
+ONE = '''
+       .-------.
+      /       /|
+     /_______/ |
+     |       | |
+     |   o   | /
+     |       |/ 
+jgs  '-------'  
+'''
+
+TWO = '''
+       .-------.
+      /       /|
+     /_______/ |
+     | o     | |
+     |       | /
+     |     o |/ 
+jgs  '-------'  
+'''
+
+THREE = '''
+       .-------.
+      /       /|
+     /_______/ |
+     | o     | |
+     |   o   | /
+     |     o |/ 
+jgs  '-------'  
+'''
+
+FOUR = '''
+       .-------.
+      /       /|
+     /_______/ |
+     | o   o | |
+     |       | /
+     | o   o |/ 
+jgs  '-------'  
+'''
+
+FIVE = '''
+       .-------.
+      /       /|
+     /_______/ |
+     | o   o | |
+     |   o   | /
+     | o   o |/ 
+jgs  '-------'  
+'''
+
+SIX = '''
+       .-------.
+      /       /|
+     /_______/ |
+     | o   o | |
+     | o   o | /
+     | o   o |/ 
+jgs  '-------'  
+'''
+
+
+print(titleart)
+print("\n")
+# zero()
+num = random.randint(1,6)
+print(" *** YOU ROLLED A {} !!! ***".format(num))
+if num == 1: one()
+if num == 2: two()
+if num == 3: three()
+if num == 4: four()
+if num == 5: five()
+if num == 6: six()
+exit()
---
+++ flip_a_coin.py
@@ -0,0 +1,15 @@
+import random
+
+def heads():
+    return
+
+def tails():
+    return
+
+
+print("Flip A Coin")
+num = random.randint(1,2)
+# print(num)
+if num == 2: print("HEADS\n")
+if num == 1: print("TAILS\n")
+exit()
